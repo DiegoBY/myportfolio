51e969a2fe05f2f569ad7622c4029eed090f82dd
DiegoBY <guerbrzlego@gmail.com>
2025-10-26 15:51:18 -0300

    fix: corrigido o erro do loading onde aparecia o scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import DisplayCode from './components/DisplayCode/DisplayCode';
@@ -9,19 +11,31 @@ import Footer from './components/Footer/Footer';
 import LoadingPortfolio from './components/LoadingPortfolio/LoadingPortfolio';
 
 function App() {
+    const [fakeLoading, setFakeLoading] = useState<string>('hidden');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFakeLoading('block');
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
             <main className="relative font-baiJumjuree">
                 <LoadingPortfolio />
 
-                <Header />
-                <HomePage />
-                <DisplayCode />
-                <Work />
-                <About />
-                <Tech />
-                <Contact />
-                <Footer />
+                <div className={`${fakeLoading}`}>
+                    <Header />
+                    <HomePage />
+                    <DisplayCode />
+                    <Work />
+                    <About />
+                    <Tech />
+                    <Contact />
+                    <Footer />
+                </div>
             </main>
         </>
     );
